9ec46851c0bbb83ee503b687fca2f1679ee198c0
hp <hp@DESKTOP-1UAUEPB>
2025-05-31 16:42:56 +0530

    machine test

--- src/test/java/EXAM/DEMO.java
+++ src/test/java/EXAM/DEMO.java
@@ -1,5 +1,7 @@
 package EXAM;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -51,11 +53,89 @@ public class DEMO {
 		System.out.println(txt);
 	}
 	
+	@Test (priority = 2)
+	public void empty_credinals() {
+		
+		driver.navigate().to("https://www.saucedemo.com/v1/inventory.html");
+		
+		driver.findElement(By.id("user-name")).sendKeys(" ");
+		driver.findElement(By.id("password")).sendKeys(" ");	
+		driver.findElement(By.id("login-button")).click();
+		
+		WebElement msg1 = driver.findElement(By.xpath("//*[@id=\"login_button_container\"]/div/form/h3"));
+		String txt1 = msg1.getText();
+		System.out.println(txt1);
+	}
+	
+	@Test (priority = 3)
+	public void display_product() {
+		
+		driver.navigate().to("https://www.saucedemo.com/v1/inventory.html");
+		
+		List<WebElement>products =driver.findElements(By.className("inventory_item"));
+		s.assertTrue(products.size()>0);
+		s.assertAll();
+	}
+	
+	@Test(priority = 4)
+	public void products_shows(){
+		
+		driver.navigate().to("https://www.saucedemo.com/v1/inventory.html");
+		
+		List<WebElement>products =driver.findElements(By.className("inventory_item"));
+		
+//use for-each method
+		for(WebElement prod : products) {
+			WebElement name= driver.findElement(By.className("inventory_item_name"));
+			WebElement image = driver.findElement(By.className("inventory_item_img"));
+			WebElement price = driver.findElement(By.className("inventory_item_price"));
+			
+			s.assertTrue(name.isDisplayed());
+			s.assertTrue(image.isDisplayed());
+			s.assertTrue(price.isDisplayed());
+			s.assertAll();
+		}
+	}
+	
+	@Test(priority = 5)
+	public void Add_products(){
+		
+		driver.navigate().to("https://www.saucedemo.com/v1/inventory.html");
+//add 1st product		
+		WebElement product_1 = driver.findElement(By.xpath("//button[text()='ADD TO CART']"));
+		product_1.click();
+		
+//add 2nd product	
+		WebElement product_2 = driver.findElement(By.xpath("//button[text()='ADD TO CART']"));
+		product_2.click();
+		
+//verify count
+		WebElement cart =driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span"));
+		String count = cart.getText();
+		System.out.println(count);	
+		
+	}
+	
+	@Test(priority = 5)
+	public void Remove_products(){
+		driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/svg/path")).click();
+		
+//remove item
+		driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[1]/div[3]/div[2]/div[2]/button")).click();
+		
+//verify count
+		WebElement cart =driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a/span"));
+		String count = cart.getText();
+		System.out.println(count);
+	}
+	
+		
 	
 	
 	@AfterTest
 	public void close() {
 		driver.close();
+		
 	}
 
 } 
